10b8d879146af68dcb40e6d93d2ae1a90c317aa3
Jamie <mccannj@protonmail.com>
2019-01-19 16:51:22 +0100

    Basic terminal server and client interaction via basic prompt implemented.. cannot cd or use command history

--- client.py
+++ client.py
@@ -7,8 +7,39 @@ class TerminalUser(object):
 
     def __init__(self):
         self.target = "127.0.0.1"
-        self.port = 9999
+        self.port = 9998
+        self.history = []
+
+    def setup_client_connection(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.target, self.port))
+        # data = self.client.recv(4096)
+        # print(data.decode("utf-8"))
+
+    def use_server_terminal(self):
+        while True:
+            recv_len = 1
+            response = ""
+
+            while recv_len:
+                data = self.client.recv(4096)
+                response += data.decode("utf-8")
+
+                if recv_len < 4096:
+                    print(response, end="")
+                    break
+            try:
+                buff = input("")
+                buff += "\n"
+                self.history.append(buff)
+                buff = buff.encode("utf-8")
+                self.client.send(buff)
+            except EOFError:
+                print("")
+                break
 
 
 if __name__ == '__main__':
     user = TerminalUser()
+    user.setup_client_connection()
+    user.use_server_terminal()
--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 import socket
 import threading
+import subprocess
 
 
 class TerminalServer(object):
@@ -9,7 +10,7 @@ class TerminalServer(object):
     def __init__(self):
         self.prompt = "<BHP:#> $ "
         self.target = "127.0.0.1"
-        self.port = 9999
+        self.port = 9998
         self.max_connections = 5
 
     def setup_server(self):
@@ -21,6 +22,9 @@ class TerminalServer(object):
         self.server.listen(self.max_connections)
 
     def run_server(self):
+        print(
+            f"[*] Starting server on {self.target}:{self.port}"
+        )
         while True:
             client_socket, addr = self.server.accept()
             client_thread = threading.Thread(
@@ -29,7 +33,32 @@ class TerminalServer(object):
             client_thread.start()
 
     def client_handle(self, client_socket):
+        while True:
             client_socket.send(self.prompt.encode("utf-8"))
+            self.cmd_buff = ""
+            while "\n" not in self.cmd_buff:
+                data = client_socket.recv(1024)
+                self.cmd_buff += data.decode("utf-8")
+            # print(self.cmd_buff.rstrip())
+            self.cmd_buff = self.cmd_buff.rstrip()
+            print(self.cmd_buff)
+            cmd_output = self.run_command()
+            client_socket.send(cmd_output)
+
+    def run_command(self):
+
+        try:
+            output = subprocess.check_output(
+                self.cmd_buff, stderr=subprocess.STDOUT, shell=True
+            )
+
+        except Exception as err:
+            output = f"{self.cmd_buff} failed to execute.\r\n"
+
+        return output
+
 
 if __name__ == '__main__':
     term = TerminalServer()
+    term.setup_server()
+    term.run_server()
