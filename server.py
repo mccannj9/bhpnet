4f252aff25b1ab6b5bbc594eaf0f34793c2ca5f9
Jamie <mccannj@protonmail.com>
2019-01-21 15:12:10 +0100

    Added 'select' methods to server, which uses async select module for processing incoming connections instead of threads

--- server.py
+++ server.py
@@ -4,7 +4,8 @@ import socket
 import threading
 import subprocess
 
-import time
+import queue
+import select
 
 
 class TerminalServer(object):
@@ -19,9 +20,85 @@ class TerminalServer(object):
         self.server = socket.socket(
             socket.AF_INET, socket.SOCK_STREAM
         )
-
         self.server.bind((self.target, self.port))
         self.server.listen(self.max_connections)
+
+    def setup_server_select(self):
+        self.server = socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM
+        )
+        self.server.setblocking(0)
+        self.server.bind((self.target, self.port))
+        self.server.listen(self.max_connections)
+
+        self.inputs = [self.server]
+        self.outputs = []
+        self.msg_queues = {}
+
+    def run_server_select(self):
+        print(
+            f"[*] Starting server on {self.target}:{self.port}"
+        )
+        while self.inputs:
+            _read, _write, _except = select.select(
+                self.inputs, self.outputs, self.inputs
+            )
+            for sock in _read:
+                if sock is self.server:
+                    client_socket, addr = self.server.accept()
+                    print(
+                        f"[*] Accepted connection from: {addr[0]}:{addr[1]}"
+                    )
+                    client_socket.setblocking(0)
+                    self.inputs.append(client_socket)
+                    self.msg_queues[client_socket] = queue.Queue()
+                    prompt_length = f"{len(self.prompt)}|".encode("utf-8")
+                    client_socket.send(prompt_length + self.prompt)
+                else:
+                    ip, port = sock.getsockname()
+                    fail = self.client_handle_select(sock, ip, port)
+                    if fail:
+                        _except.append(sock)
+
+            for sock in _write:
+                try:
+                    next_msg = self.msg_queues[sock].get_nowait()
+                except queue.Empty:
+                    self.outputs.remove(sock)
+                    continue
+                sock.send(next_msg)
+
+            for sock in _except:
+                self.inputs.remove(sock)
+                if sock in self.outputs:
+                    self.outputs.remove(sock)
+                sock.close()
+                del self.msg_queues[sock]
+
+    def client_handle_select(
+        self, client_socket, client_ip, client_port
+    ):
+        cmd_buff = ""
+        while "\n" not in cmd_buff:
+            data = client_socket.recv(1024).decode("utf-8")
+            if data == "x404x":
+                ip, port = client_socket.getpeername()
+                print(
+                    f"[*] Client connection closed: {ip}:{port}"
+                )
+                return client_socket
+            else:
+                cmd_buff += data
+
+        cmd_buff = cmd_buff.rstrip()
+        cmd_output = self.run_command_select(cmd_buff) + self.prompt
+        msg_length = f"{len(cmd_output)}|".encode("utf-8")
+        print(f"{cmd_buff} {len(cmd_output)}")
+        cmd_output = msg_length + cmd_output
+        self.msg_queues[client_socket].put(cmd_output)
+        if client_socket not in self.outputs:
+            self.outputs.append(client_socket)
+        return None
 
     def run_server(self):
         print(
@@ -48,7 +125,7 @@ class TerminalServer(object):
                     print(f"joining {t.ident}")
                     t.join()
                     threads.pop(t)
-            time.sleep(1)
+            # time.sleep(1)
 
     def close_server(self):
         self.server.shutdown(socket.SHUT_RDWR)
@@ -69,11 +146,9 @@ class TerminalServer(object):
                 if self.cmd_buff == "x404x":
                     exit_code_received = True
                     print(
-                        f"[*] Client connection closed: "
+                        "[*] Client connection closed: "
                         f"{client_ip}:{client_port}"
                     )
-
-                    self.cmd_buff
                     break
 
             if exit_code_received:
@@ -93,6 +168,16 @@ class TerminalServer(object):
 
         return 0
 
+    def run_command_select(self, cmd):
+        try:
+            output = subprocess.check_output(
+                cmd, stderr=subprocess.STDOUT, shell=True
+            )
+        except Exception as err:
+            output = f"{cmd} failed to execute.\n".encode("utf-8")
+
+        return output
+
     def run_command(self):
         try:
             output = subprocess.check_output(
@@ -110,8 +195,10 @@ def main():
 
     try:
         term = TerminalServer("127.0.0.1", 9998)
-        term.setup_server()
-        term.run_server()
+        # term.setup_server()
+        # term.run_server()
+        term.setup_server_select()
+        term.run_server_select()
     except KeyboardInterrupt:
         term.close_server()
     except Exception:
